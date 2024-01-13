8f4365110fbbaec8d4ecf5195577bcf50340c895
User <User@MEE>
2024-01-13 23:29:02 +0100

    Ajoutez des fichiers projet.

---
+++ testing/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace testing.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
